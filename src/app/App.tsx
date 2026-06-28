be9f2ecd5551a1e8f84407deaf9d538e83260161
OMAR <OMAR@comets.com>
2026-06-28 20:37:59 +0900

    feat: MUI standart integration

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
 
 import '../css/app.css';
+import { Box, Button, Container, Stack, Typography } from '@mui/material';
+
 
 function App() {
-  return <div>Home</div>
-}
+  return (
+    <Container maxWidth="sm">
+      <Stack>
+        <Box sx={{my:4}}>
+          <Typography variant='h4' component={"h4"}>
+             Create React app 
 
+          </Typography>
+        </Box>
+        <Button variant='contained'> click me</Button>
+        <Button variant="contained">Contained</Button>
+        <Button variant="outlined">Outlined</Button>
+
+      </Stack>
+    </Container>
+
+    
+)}
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,20 @@ import  ReactDOM  from 'react-dom';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './app/App';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
 import reportWebVitals from './reportWebVitals';
 import './css/index.css';
+import theme from '../src/theme';
 
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline/>
       <App />
+      </ThemeProvider >
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')

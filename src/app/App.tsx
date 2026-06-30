20eb7f113c5ba1de684ff9a5564144f13fb6a7de
OMAR <OMAR@comets.com>
2026-06-30 19:52:50 +0900

    feat: develop burak screen based components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,47 +2,50 @@ import React from 'react';
 
 import '../css/app.css';
 import { Router, Switch , Route , Link , } from 'react-router-dom';
-import { About } from './screens/About';
-import { Users } from './screens/Users';
-
+import { HomePage } from './screens/homePage';
+import { ProductsPage } from './screens/productsPage';
+import { UsersPage } from './screens/userPage';
+import { OrdersPage } from './screens/ordersPage';
 
 function App() {
   return (
       <div>
         <nav>
           <ul>
-            <li>
-              <Link to="/">Home</Link>
+             <li>
+              <Link to="/">HomePage</Link>
             </li>
             <li>
-              <Link to="/about">About</Link>
+              <Link to="/products">ProductsPage</Link>
             </li>
             <li>
-              <Link to="/users">Users</Link>
+              <Link to="/orders">OrdersPage</Link>
+            </li>
+            <li>
+              <Link to="/member-page">UserPage</Link>
             </li>
           </ul>
         </nav>
 
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
         <Switch>
-          <Route path="/about">
-            <About />
+          <Route path="/products">
+            <ProductsPage />
           </Route>
-          <Route path="/users">
-            <Users />
+          <Route path="/orders">
+            <OrdersPage/>
+          </Route>
+          <Route path="/member-page">
+            <UsersPage />
           </Route>
           <Route path="/">
-            <Home />
+            <HomePage />
           </Route>
         </Switch>
       </div>
 
     
 )
-function Home() {
-  return <h2>Home</h2>;
-}
+
 
 }
 export default App;

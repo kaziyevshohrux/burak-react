35ef9e1220e2c41878c4b6c847b806c28976c1ee
OMAR <OMAR@comets.com>
2026-07-25 13:39:44 +0900

    feat: create config and env variables

---
+++ src/lib/config.ts
@@ -0,0 +1,12 @@
+import { error } from "console"
+
+export const serverApi : string = `${process.env.REACT_APP_API_URL }` 
+
+export const Message = {
+    error1: "Something went wrong, please try again later.",
+    error2: "Please login first!",
+    error3: "Please fullfil all inputs!",
+    error4:"Message is empty!",
+    error5: "only images with jpeg, jpg, png fromat allowes",
+
+}

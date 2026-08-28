18cc7e78efd729525203676e9cd871e284982f28
OMAR <OMAR@comets.com>
2026-08-28 21:48:55 +0900

    feat: BRR Initial setup

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
 import '../css/app.css';
+import { Button } from '@mui/material';
 
 function App() {
-  return <div>Home</div>
+  return <Button variant="contained">
+	Login
+</Button>
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
-import  ReactDOM  from 'react-dom';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
+import ReactDOM from 'react-dom/client';
+import { ThemeProvider, CssBaseline } from '@mui/material';
 import App from './app/App';
+import MaterialTheme from './app/MaterialTheme';
 import reportWebVitals from './reportWebVitals';
-import './css/index.css';
 
+const root = ReactDOM.createRoot(
+	document.getElementById('root') as HTMLElement,
+);
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+root.render(
+	<React.StrictMode>
+		<ThemeProvider theme={MaterialTheme}>
+			<CssBaseline />
+			<App/>
+		</ThemeProvider>
+	</React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
